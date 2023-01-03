82781e841e6d0719f15c165a303a96f5e6ba6c18
hongmaoxiao <769501294@qq.com>
2023-01-04 07:38:10 +0800

    add Create random passwords from a set of user-defined characters

--- Algorithms/random_password/src/main.rs
+++ Algorithms/random_password/src/main.rs
@@ -8,4 +8,19 @@ fn main() {
         .collect();
 
     println!("{}", rand_string);
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = thread_rng();
+    
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    println!("{:?}", password);
 }
